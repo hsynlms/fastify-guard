f7480fa844be7e15492dc67a3a38fb488faefc83
Francis Daigle <francis@stetsolutions.com>
2021-05-17 14:24:50 -0700

    Add TS declaration file

---
+++ types/index.d.ts
@@ -0,0 +1,35 @@
+import createHttpError from 'http-errors'
+import {
+  FastifyPluginCallback,
+  FastifyReply,
+  FastifyRequest,
+  preHandlerHookHandler
+} from 'fastify'
+
+interface FastifyGuard {
+  hasRole(
+    request: FastifyRequest,
+    role: string
+  ): boolean | createHttpError.HttpError
+  hasScope(
+    request: FastifyRequest,
+    scope: string
+  ): boolean | createHttpError.HttpError
+  role(roles: string[]): preHandlerHookHandler
+  scope(scopes: string[]): preHandlerHookHandler
+}
+
+declare module 'fastify' {
+  interface FastifyInstance {
+    guard: FastifyGuard
+  }
+}
+
+declare const fastifyGuard: FastifyPluginCallback<{
+  errorHandler(
+    result: createHttpError.HttpError,
+    request: FastifyRequest,
+    reply: FastifyReply
+  ): any
+}>
+export default fastifyGuard
